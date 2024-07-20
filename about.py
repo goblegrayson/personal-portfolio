20ae8a78680ba001237ce8fdfecd4682c14ac9d4
Grayson Goble <goble.develop@gmail.com>
2024-07-19 20:48:35 -0400

    - couple content tweaks

--- about.py
+++ about.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import streamlit as st
 
-# Read file
-file_name = 'aboutme.md'
-file_path = Path('markdown', file_name)
-with open(file_path, 'r') as file:
-    text = file.read()
 
-# Now print it
-st.markdown(body=text)
+def load_markdown(file_name):
+    file_path = Path(__file__).parent.joinpath('markdown', file_name)
+    with open(file_path, 'r') as file:
+        return file.read()
+
+
+st.markdown(body=load_markdown('aboutme.md'), unsafe_allow_html=True)
